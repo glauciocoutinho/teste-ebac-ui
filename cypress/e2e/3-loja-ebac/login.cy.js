42b7fb28da9635bdda2a6ac777960ac114739ced
Glaucio Coutinho <glauciocoutinhodasilva@gmail.com>
2024-02-19 06:13:59 -0400

    Testes negativos e Hooks

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,13 +1,39 @@
-///<reference types="cypress"/>
+/// <reference types="cypress"/>
 
 describe('Funcionalidade: Login', () => {
 
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')        
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
     it('Deve fazer login com sucesso', () => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('coutox2000@gmail.com')
         cy.get('#password').type('Ebacshop@10')
         cy.get('.woocommerce-form > .button').click()
-
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, coutox2000 (não é coutox2000? Sair)')
-    })
+    
+    });
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.get('#username').type('coutox@gmail.com')
+        cy.get('#password').type('Ebacshop@10')
+        cy.get('.woocommerce-form > .button').click()
+        //cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido.')
+        cy.get('.woocommerce-error').should('exist')
+
+    });
+
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+        cy.get('#username').type('coutox2000@gmail.com')
+        cy.get('#password').type('Ebacsho@10')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail coutox2000@gmail.com está incorreta. Perdeu a senha?')
+        cy.get('.woocommerce-error').should('exist')
+        
+    });
+
 })
